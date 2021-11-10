f8d9e34f22f1c68ae0512fc71703be4682b59079
Robin Dittrich <robinodittrich@gmail.com>
2021-11-09 17:43:54 -0800

    Complete adult? drive and driver methods

---
+++ lib/passenger.rb
@@ -0,0 +1,25 @@
+class Passenger
+  attr_reader :name, :age, :adult, :driver
+  def initialize(attributes)
+    @name = attributes["name"]
+    @age = attributes["age"]
+    @adult = false
+    @driver = false
+  end
+
+  def adult?
+    if @age >= 18
+      @adult = true
+    else
+      @adult = false
+    end
+  end
+
+  def drive
+    @driver = true
+  end
+
+  def driver?
+    @driver
+  end
+end
