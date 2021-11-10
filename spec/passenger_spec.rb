34f562ac81c298467698bbb7812725e998c0c6e1
Robin Dittrich <robinodittrich@gmail.com>
2021-11-09 17:22:14 -0800

    Create passenger test file

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,12 @@
+require './lib/passenger'
+
+RSpec.describe Passenger do
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'exists' do
+    expect(@charlie).to be_instance_of(Passeger)
+  end
+end
